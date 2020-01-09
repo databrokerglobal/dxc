af465344c9d8deabef961c962a0cd7464f95b125
goosecoid <adblavier@gmail.com>
2020-01-09 14:40:10 +0100

    feat: unit test for hash comparison

---
+++ filemanager/file-parser_test.go
@@ -0,0 +1,26 @@
+package filemanager
+
+import "testing"
+
+func Test_compareHashes(t *testing.T) {
+	type args struct {
+		file1 []byte
+		file2 []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Case 1: two exact same strings", args{[]byte("test"), []byte("test")}, true},
+		{"Case 2: two exact same strings", args{[]byte("foo"), []byte("bar")}, false},
+		{"Case 3: double nil", args{[]byte(nil), []byte(nil)}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compareHashes(tt.args.file1, tt.args.file2); got != tt.want {
+				t.Errorf("compareHashes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
