e25a6017ee5235328c82c662fdd218d6f6d179a4
goosecoid <adblavier@gmail.com>
2020-01-09 11:08:08 +0100

    feat: env vars

--- files/files.go
+++ files/files.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
@@ -31,8 +32,22 @@ func Upload(c echo.Context) error {
 	}
 	defer src.Close()
 
-	// Open same file in the mounted docker volume
-	from, err := os.Open(fmt.Sprintf("/var/files/%s", file.Filename))
+	// Load env files
+	err = godotenv.Load()
+	if err != nil {
+		return err
+	}
+
+	// Load file path
+	var filePath string
+	if os.Getenv("GO_ENV") == "development" {
+		filePath = os.Getenv("LOCAL_FILES_DIR")
+	} else {
+		filePath = "/var/files"
+	}
+
+	// Open same file in the mounted docker volume (or just local dir if go_env=development)
+	from, err := os.Open(fmt.Sprintf("%s/%s", filePath, file.Filename))
 	if err != nil {
 		return err
 	}
