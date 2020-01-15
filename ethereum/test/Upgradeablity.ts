018724618f8220d0c26479db7bdb893a5c4c189b
goosecoid <adblavier@gmail.com>
2020-01-15 16:51:41 +0100

    feat: fully tested upgradeability

--- ethereum/test/Upgradeablity.ts
+++ ethereum/test/Upgradeablity.ts
@@ -41,9 +41,54 @@ contract(
         })
         .catch(err => err);
 
+      // if correctly initialized, then a second run throws an error
       assert.equal(
         String(err).includes('Error: Transaction reverted without a reason'),
         true
+      );
+
+      // check if data is accessible through the proxy
+      const logicContractFromProxy = await Token.at(proxy.address);
+      assert.equal(await logicContractFromProxy.owner(), accounts[0]);
+    });
+
+    it('after upgrading data should remain intact', async () => {
+      const proxy = await Proxy.new();
+      const token = await Token.new();
+
+      const initData = encodeCall('initialize', ['address'], [accounts[0]]);
+
+      assert.isOk(
+        await proxy.upgradeToAndCall(token.address, initData, {
+          from: accounts[0],
+        })
+      );
+
+      const logicContractFromProxy = await Token.at(proxy.address);
+      await logicContractFromProxy.mint(accounts[0], 100);
+
+      const newToken = await TokenUpgraded.new();
+
+      assert.isOk(await proxy.upgradeTo(newToken.address));
+
+      const upgradedLogicContractFromProxy = await TokenUpgraded.at(
+        proxy.address
+      );
+
+      assert.equal(
+        await (
+          await upgradedLogicContractFromProxy.balanceOf(accounts[0])
+        ).toNumber(),
+        100
+      );
+
+      await upgradedLogicContractFromProxy.burn(50);
+
+      assert.equal(
+        await (
+          await upgradedLogicContractFromProxy.balanceOf(accounts[0])
+        ).toNumber(),
+        50
       );
     });
   }
