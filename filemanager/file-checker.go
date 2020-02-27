ffcf16bb50795e7a2fc161d6e99246e4724b8bd3
goosecoid <adblavier@gmail.com>
2020-02-27 14:48:28 +0100

    fix: file checker restores files now

--- filemanager/file-checker.go
+++ filemanager/file-checker.go
@@ -14,6 +14,14 @@ func init() {
 		log.Println("Testing: omitting database init")
 		return
 	}
+
+	// Start up a go routine for file checking
+	go CheckingFiles()
+}
+
+// CheckingFiles Delete files that are not in directory anymore, restore files that were deleted but now back in their directory
+func CheckingFiles() {
+
 	fmt.Println("Checking file integrity...")
 	files, err := database.DBInstance.GetFiles()
 	if err != nil {
@@ -25,5 +33,20 @@ func init() {
 			database.DBInstance.DeleteFile(file.Name)
 		}
 	}
+
+	// Checking if previously deleted files are back up
+	var deletedFiles []database.File
+	// Unscoped includes soft deleted files in the query
+	// Raw allows you to do SQL queries directly
+	database.DBInstance.DB.Unscoped().Raw(`SELECT * FROM files WHERE deleted_at IS NOT NULL`).Find(&deletedFiles)
+
+	for _, deletedFile := range deletedFiles {
+		_, err = open(deletedFile.Name)
+		if err == nil {
+			// if file opened successfully remove the soft deletion
+			database.DBInstance.DB.Unscoped().Model(&deletedFile).Update("deleted_at", nil)
+		}
+	}
+
 	defer fmt.Println("Finished checking file integrity")
 }
