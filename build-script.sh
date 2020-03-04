a151aacd6f8f3231bc46cb53805183973dedc0cd
goosecoid <adblavier@gmail.com>
2020-03-04 10:44:30 +0100

    fix: cleanup before build + copy env file

--- build-script.sh
+++ build-script.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+rm -rf "dxc_build_$(date +%Y%m%d)" &&
+rm -rf build &&
 mkdir "dxc_build_$(date +%Y%m%d)" &&
 go build -o "dxc_build_$(date +%Y%m%d)"/dxc &&
 touch "dxc_build_$(date +%Y%m%d)"/dxc.db &&
+cp .env-example "dxc_build_$(date +%Y%m%d)"/.env &&
 cd ui &&
 npm i &&
 npm run build &&
