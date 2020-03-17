56103f33a450847df6aef41e9b7994fb8d76fddf
goosecoid <adblavier@gmail.com>
2020-03-17 17:11:26 +0100

    fix: update migrations

--- ethereum/migrations/3_deploy_dxc.ts
+++ ethereum/migrations/3_deploy_dxc.ts
@@ -1,15 +1,37 @@
-import {DXCContract} from '../types/truffle-contracts';
+import {encodeCall} from '../test/utils/encodeCall';
+import {
+  DTXTokenContract,
+  DTXTokenInstance,
+  DXCContract,
+  OwnedUpgradeabilityProxyContract,
+} from '../types/truffle-contracts';
 
-//const DTXMiniMe: DTXTokenContract = artifacts.require('DTXToken');
+const DTXMiniMe: DTXTokenContract = artifacts.require('DTXToken');
 const DXC: DXCContract = artifacts.require('DXC');
+const Proxy: OwnedUpgradeabilityProxyContract = artifacts.require(
+  'OwnedUpgradeabilityProxy'
+);
 
 const performMigration = async (
   deployer: Truffle.Deployer,
   network: string,
   accounts: string[]
 ) => {
-  //const dTXTokenInstance: DTXTokenInstance = await DTXMiniMe.deployed();
+  const dTXTokenInstance: DTXTokenInstance = await DTXMiniMe.deployed();
+
   await deployer.deploy(DXC);
+  await deployer.deploy(Proxy);
+
+  const dDxc = await DXC.deployed();
+  const dProxy = await Proxy.deployed();
+
+  const data = encodeCall(
+    'initialize',
+    ['address'],
+    [dTXTokenInstance.address]
+  );
+
+  await dProxy.upgradeToAndCall(dDxc.address, data, {from: accounts[0]});
 };
 
 module.exports = (deployer: any, network: string, accounts: string[]) => {
