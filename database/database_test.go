0dad3aefc60d6db2302675fb24c8d81c619831ef
Jon <woodyjon@gmail.com>
2020-06-15 16:49:53 +0200

    test create syncstatus, infuraID, userauth

--- database/database_test.go
+++ database/database_test.go
@@ -85,3 +85,73 @@ func (s *Suite) TestGetDatasourceByDID() {
 	require.NoError(s.T(), err)
 	require.Nil(s.T(), deep.Equal(datasource, returnedDatasource))
 }
+
+func (s *Suite) TestCreateSyncStatus() {
+
+	syncStatus := &SyncStatus{
+		Success:    true,
+		ErrorResp:  "no message",
+		StatusCode: 200,
+		Status:     "SUCCESS 200",
+	}
+
+	s.mock.ExpectBegin()
+	s.mock.ExpectExec(`INSERT INTO "sync_statuses"`).WithArgs(AnyTime{}, AnyTime{}, nil, syncStatus.Success, syncStatus.ErrorResp, syncStatus.StatusCode, syncStatus.Status).WillReturnResult(sqlmock.NewResult(0, 1))
+	s.mock.ExpectCommit()
+
+	err := s.repository.CreateSyncStatus(syncStatus.Success, syncStatus.ErrorResp, syncStatus.StatusCode, syncStatus.Status)
+
+	require.NoError(s.T(), err)
+}
+
+func (s *Suite) TestSaveNewUserAuth() {
+
+	userAuth := &UserAuth{
+		Address: "0x1111",
+		APIKey:  "1234",
+	}
+
+	s.mock.ExpectBegin()
+	s.mock.ExpectExec(`INSERT INTO "user_auths"`).WithArgs(AnyTime{}, AnyTime{}, nil, userAuth.Address, userAuth.APIKey).WillReturnResult(sqlmock.NewResult(0, 1))
+	s.mock.ExpectCommit()
+
+	err := s.repository.SaveNewUserAuth(userAuth.Address, userAuth.APIKey)
+
+	require.NoError(s.T(), err)
+}
+
+func (s *Suite) TestCreateInfuraID() {
+
+	infuraIDObject := &InfuraID{
+		InfuraID: "1234",
+	}
+
+	s.mock.ExpectBegin()
+	s.mock.ExpectExec(`INSERT INTO "infura_ids"`).WithArgs(AnyTime{}, AnyTime{}, nil, infuraIDObject.InfuraID).WillReturnResult(sqlmock.NewResult(0, 1))
+	s.mock.ExpectCommit()
+
+	err := s.repository.CreateInfuraID(infuraIDObject.InfuraID)
+
+	require.NoError(s.T(), err)
+}
+
+func (s *Suite) TestGetLatestInfuraID() {
+
+	infuraIDObject1 := &InfuraID{
+		InfuraID: "1234",
+	}
+	infuraIDObject2 := &InfuraID{
+		InfuraID: "2345",
+	}
+
+	s.mock.ExpectQuery(
+		regexp.QuoteMeta(
+			`SELECT * FROM "infura_ids" WHERE "infura_ids"."deleted_at" IS NULL ORDER BY created_at desc,"infura_ids"."id" ASC LIMIT 1`,
+		),
+	).WillReturnRows(sqlmock.NewRows([]string{"infura_id"}).AddRow(infuraIDObject1.InfuraID).AddRow(infuraIDObject2.InfuraID))
+
+	returnedInfuraID, err := s.repository.GetLatestInfuraID()
+
+	require.NoError(s.T(), err)
+	require.Nil(s.T(), deep.Equal(infuraIDObject2.InfuraID, returnedInfuraID)) // check that the last one is returned (2) not the first one.
+}
