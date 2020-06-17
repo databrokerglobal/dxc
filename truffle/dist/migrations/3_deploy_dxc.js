af4975042ab8446905f90fa4535bef63f28a979d
goosecoid <adblavier@gmail.com>
2020-06-17 08:51:03 +0200

    fix: all tests passing + remove dxc.sol

--- truffle/dist/migrations/3_deploy_dxc.js
+++ truffle/dist/migrations/3_deploy_dxc.js
@@ -1,24 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const DTXMiniMe = artifacts.require('DTXToken');
-const DXC = artifacts.require('DXC');
-const Proxy = artifacts.require('OwnedUpgradeabilityProxy');
+const DXCTokens = artifacts.require('DXCTokens');
+const DXCDeals = artifacts.require('DXCDeals');
+const ProxyDeals = artifacts.require('OwnedUpgradeabilityProxy');
+const ProxyTokens = artifacts.require('OwnedUpgradeabilityProxy');
 const performMigration = async (deployer, network, accounts) => {
     const dTXTokenInstance = await DTXMiniMe.deployed();
     // We are going to deploy the DXC using a proxy pattern, allowing us to upgrade the DXC contract later
-    await deployer.deploy(DXC);
-    await deployer.deploy(Proxy);
-    const dDxc = await DXC.deployed();
-    await dDxc.initialize(dTXTokenInstance.address);
-    // const dProxy = await Proxy.deployed();
-    // // encode the calling of the initializer, which here acts as the constructor for the DXC contract
-    // const data = encodeCall(
-    //   'initialize',
-    //   ['address'],
-    //   [dTXTokenInstance.address]
-    // );
-    // // point proxy to DXC contract and call the constructor (aka the initializer)
-    // await dProxy.upgradeToAndCall(dDxc.address, data, {from: accounts[0]});
+    await deployer.deploy(DXCTokens);
+    await deployer.deploy(DXCDeals);
+    await deployer.deploy(ProxyDeals);
+    await deployer.deploy(ProxyTokens);
+    const dDxcTokens = await DXCTokens.deployed();
+    const dDxcDeals = await DXCDeals.deployed();
+    const dProxyTokens = await ProxyTokens.deployed();
+    const dProxyDeals = await ProxyDeals.deployed();
+    await dProxyTokens.upgradeTo(dDxcTokens.address);
+    await dProxyTokens.initialize(dTXTokenInstance.address, dProxyDeals.address);
+    await dProxyDeals.upgradeTo(dDxcDeals.address);
+    await dProxyDeals.initialize(dProxyTokens.address);
 };
 module.exports = (deployer, network, accounts) => {
     deployer
--- truffle/test/upgradeability.ts
+++ truffle/test/upgradeability.ts
@@ -13,12 +13,10 @@ import {
   OwnedUpgradeabilityProxyInstance,
 } from '../types/truffle-contracts';
 
-import {encodeCall} from './utils/encodeCall';
-
 const TF: MiniMeTokenFactoryContract = artifacts.require('MiniMeTokenFactory');
 const DTX: DTXTokenContract = artifacts.require('DTXToken');
 const DXCDeals: DXCDealsContract = artifacts.require('DXCDeals');
-const DXCTokens: DXCTokensContract = artifacts.require('DXC');
+const DXCTokens: DXCTokensContract = artifacts.require('DXCTokens');
 const DXCV2: DXCV2Contract = artifacts.require('DXCV2');
 const OUP: OwnedUpgradeabilityProxyContract = artifacts.require(
   'OwnedUpgradeabilityProxy'
@@ -33,28 +31,31 @@ contract('Upgradeability of DXC', async accounts => {
     const oUPDealsinstance: OwnedUpgradeabilityProxyInstance = await OUP.new();
     const oUPTokensinstance: OwnedUpgradeabilityProxyInstance = await OUP.new();
 
-    // Encode the calling of the function initialize with the argument dtxInstance.address to bytes
-    const data = encodeCall('initialize', ['address'], [dtxInstance.address]);
+    let proxiedTokensDxc: DXCTokensInstance;
+    let proxiedDealsDxc: DXCDealsInstance;
 
-    // point proxy contract to dxc contract and call the initialize function which is analogous to a constructor
     assert.isOk(
-      await oUPDealsinstance.upgradeToAndCall(dxcDealsInstance.address, data, {
+      await oUPTokensinstance.upgradeTo(dxcTokensInstance.address, {
         from: accounts[0],
       })
     );
 
     assert.isOk(
-      await oUPTokensinstance.upgradeToAndCall(
-        dxcTokensInstance.address,
-        data,
-        {
-          from: accounts[0],
-        }
-      )
+      await oUPDealsinstance.upgradeTo(dxcDealsInstance.address, {
+        from: accounts[0],
+      })
     );
 
-    // Intitialize the proxied dxc instance
-    const proxiedTokensDxc = await DXCTokens.at(oUPTokensinstance.address);
+    proxiedTokensDxc = await DXCTokens.at(oUPTokensinstance.address);
+    proxiedDealsDxc = await DXCDeals.at(oUPDealsinstance.address);
+
+    await proxiedTokensDxc.initialize(
+      dtxInstance.address,
+      proxiedDealsDxc.address
+    );
+
+    await proxiedDealsDxc.initialize(proxiedTokensDxc.address);
+
     // check if the intial state is correct
     const val2 = await proxiedTokensDxc.protocolPercentage();
     assert.equal(val2.toNumber(), 5);
@@ -69,30 +70,48 @@ contract('Upgradeability of DXC', async accounts => {
     const tfInstance: MiniMeTokenFactoryInstance = await TF.new();
     const dtxInstance: DTXTokenInstance = await DTX.new(tfInstance.address);
     const dxcTokensInstance: DXCTokensInstance = await DXCTokens.new();
-    const oUPinstance: OwnedUpgradeabilityProxyInstance = await OUP.new();
+    const dxcDealsInstance: DXCDealsInstance = await DXCDeals.new();
+    const oUPTokensinstance: OwnedUpgradeabilityProxyInstance = await OUP.new();
+    const oUPDealsinstance: OwnedUpgradeabilityProxyInstance = await OUP.new();
 
-    const data = encodeCall('initialize', ['address'], [dtxInstance.address]);
+    let proxiedTokensDxc: DXCTokensInstance;
+    let proxiedDealsDxc: DXCDealsInstance;
 
     assert.isOk(
-      await oUPinstance.upgradeToAndCall(dxcTokensInstance.address, data, {
+      await oUPTokensinstance.upgradeTo(dxcTokensInstance.address, {
         from: accounts[0],
       })
     );
 
-    const proxiedDxc = await DXCTokens.at(oUPinstance.address);
-    const val2 = await proxiedDxc.protocolPercentage();
+    assert.isOk(
+      await oUPDealsinstance.upgradeTo(dxcDealsInstance.address, {
+        from: accounts[0],
+      })
+    );
+
+    proxiedTokensDxc = await DXCTokens.at(oUPTokensinstance.address);
+    proxiedDealsDxc = await DXCDeals.at(oUPDealsinstance.address);
+
+    await proxiedTokensDxc.initialize(
+      dtxInstance.address,
+      proxiedDealsDxc.address
+    );
+
+    await proxiedDealsDxc.initialize(proxiedTokensDxc.address);
+
+    const val2 = await proxiedTokensDxc.protocolPercentage();
     assert.equal(val2.toNumber(), 5);
 
-    await proxiedDxc.changeProtocolPercentage(10);
-    const val3 = await proxiedDxc.protocolPercentage();
+    await proxiedTokensDxc.changeProtocolPercentage(10);
+    const val3 = await proxiedTokensDxc.protocolPercentage();
     assert.equal(val3.toNumber(), 10);
 
     // deploy new version of DXC with the newFeature method
     const newDxcInstance: DXCV2Instance = await DXCV2.new();
-    assert.isOk(await oUPinstance.upgradeTo(newDxcInstance.address));
+    assert.isOk(await oUPTokensinstance.upgradeTo(newDxcInstance.address));
 
     // Check if state of previous dxcInstance is still maintained
-    const proxiedUpgradedDxc = await DXCV2.at(oUPinstance.address);
+    const proxiedUpgradedDxc = await DXCV2.at(oUPTokensinstance.address);
     const val4 = await proxiedUpgradedDxc.protocolPercentage();
     assert.equal(val4.toNumber(), 10);
 
