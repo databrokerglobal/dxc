8d58abfbc81017f8b4c881d37f3bf4d2c06ff30d
Gyanendra Sharma <61640241+gyan-sharma@users.noreply.github.com>
2021-02-19 05:19:59 +0530

    env CC=arm-linux-gnueabihf-gcc

--- build-script-arm32v7.sh
+++ build-script-arm32v7.sh
@@ -10,7 +10,7 @@ mkdir ${BUILD_DIR}
 env CC=arm-none-eabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=7
 go build -o ${BUILD_DIR}/dxc
 cd databroker-signature
-env CC=arm-none-eabi-gcc GOOS=linux GOARCH=arm GOARM=7
+env CC=arm-linux-gnueabihf-gcc GOOS=linux GOARCH=arm GOARM=7
 go build -o databroker-signature
 cd ..
 mv ./databroker-signature/databroker-signature ${BUILD_DIR}
