6b8a24d49937605000de21bbcca5220d86418583
Ashlesh Sortee <ashlesh@settlemint.com>
2021-05-29 17:15:58 +0530

    Remove unnecessary files from truffle-testnet

--- truffle-testnet/migrations/2_dxc_deals.ts
+++ truffle-testnet/migrations/2_dxc_deals.ts
@@ -1,14 +1,14 @@
-const {  deployProxy } = require('@openzeppelin/truffle-upgrades');
-import { DXCDealsContract } from '../types/truffle-contracts';
-const DXCDeals:DXCDealsContract  = artifacts.require('./DXCDeals.sol');
+const {deployProxy} = require("@openzeppelin/truffle-upgrades");
+import {DXCDealsContract} from "../types/truffle-contracts";
+const DXCDeals: DXCDealsContract = artifacts.require("./DXCDeals.sol");
 
-module.exports = async function (deployer: any) {
+module.exports = async function(deployer: any) {
   const lockPeriod = 30;
   const platformPercentage = 10;
   const instance = await deployProxy(
     DXCDeals,
     [lockPeriod, platformPercentage],
-    { deployer, initializer: 'initialize' }
+    {deployer, initializer: "initialize"}
   );
-  console.log('Deployed', instance.address);
+  console.log("Deployed", instance.address);
 };
